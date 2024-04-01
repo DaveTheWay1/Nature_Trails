3debbd31d8c837ec7ae5afe3f6345a2ae7bc2cf2
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-31 19:08:53 -0700

    Remove folder from tracking

--- flask_app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-UPLOAD_FOLDER = '/Users/ramirez-vazquez/Desktop/python_project/views/flask_app/uploads'
-
-
-app = Flask(__name__)
-app.secret_key = "hello"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
